c635e9702e352d8c24ee79eaa361ca47ca6c2f29
Alex <alexandrbolshakov@yahoo.com>
2020-04-15 13:15:12 +0300

    second task

--- script.js
+++ script.js
@@ -11,12 +11,28 @@ let appData = {
     savings : false
 };
 
-let oneOne = prompt("Введите обязательную статью расходов в этом месяце","");
-let oneTwo = prompt("Во сколько обойдется?","");
-let twoOne = prompt("Введите обязательную статью расходов в этом месяце","");
-let twoTwo = prompt("Во сколько обойдется?","");
-appData.expenses[oneOne] = oneTwo;
-appData.expenses[twoTwo] = twoTwo;
 
-let result = appData.budget/30;
-alert(result);
+for(let i = 0; i<2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце","");
+    let b = +prompt("Во сколько обойдется?","");
+    if((typeof(a))==='string' && typeof(a) != null &&typeof(b) != null && a!='' && b!='' && a.length<50 ){
+        console.log('done');
+        appData.expenses[a] = b;
+    }else{
+        i--;
+    }
+    
+}
+
+appData.moneyPerDay = appData.budget/30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100){
+    console.log("Минимальный урвень дохода");
+}else if(appData.moneyPerDay > 100 && appData.moneyPerDay <2000){
+    console.log("Средний урвень дохода");
+}else if(appData.moneyPerDay > 2000){
+    console.log("Высокий урвень дохода");
+}else{
+    console.log("Произошла ошибка");
+}
