9776e7f19510e693df66dabfcb18ad98ffed1218
Alex <alexandrbolshakov@yahoo.com>
2020-04-15 11:34:32 +0300

    first task

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// confirm("are you sure?")
-let answer = prompt("insert message", "text");
-// console.log(answer);
+'use script';
 
-// let answer = confirm("are you sure?");
+let money = prompt("Ваш бюджет на месяц?", ""); 
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+    budget : money,
+    timeDate : time,
+    expenses : {},
+    optionalExpenses : {},
+    savings : false
+};
+
+let oneOne = prompt("Введите обязательную статью расходов в этом месяце","");
+let oneTwo = prompt("Во сколько обойдется?","");
+let twoOne = prompt("Введите обязательную статью расходов в этом месяце","");
+let twoTwo = prompt("Во сколько обойдется?","");
+appData.expenses[oneOne] = oneTwo;
+appData.expenses[twoTwo] = twoTwo;
+
+let result = appData.budget/30;
+alert(result);
